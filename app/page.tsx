78b9206a380e80db288cf6caa1cc2ed26374886d
Max K <theZnat7@yandex.ru>
2025-04-17 19:54:35 +0300

    add filter tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,44 @@
+// app/page.tsx
 import PostsMainPage from "@/components/PostsMainPage/PostsMainPage";
+import TagsFilter from "@/components/TagsFilter/TagsFilter";
 import { Metadata } from "next";
+import "./page.css";
 
 export const metadata: Metadata = {
   title: "Главная страница | Next App",
 };
 
-const getAllPostsMain = async () => {
-  const response = await fetch("http://localhost:3000/api/posts");
+type Props = {
+  searchParams: {
+    tag?: string;
+  };
+};
+
+const getAllPostsMain = async (tag?: string) => {
+  const url = tag
+    ? `http://localhost:3000/api/posts?tag=${tag}`
+    : `http://localhost:3000/api/posts`;
+
+  const response = await fetch(url, {
+    cache: "no-store",
+  });
+
   if (!response.ok) {
     throw new Error("Unable to fetch posts");
   }
+
   const data = await response.json();
   return data.posts;
 };
 
-export default async function Home() {
-  const posts = await getAllPostsMain(); // Получаем данные с сервера
+export default async function Home({ searchParams }: Props) {
+  const posts = await getAllPostsMain(searchParams.tag);
 
   return (
     <>
       <h1>Welcome to NextJS world</h1>
-      <PostsMainPage posts={posts} /> {/* Передаем данные как пропсы */}
+      <TagsFilter />
+      <PostsMainPage posts={posts} />
     </>
   );
 }
---
+++ components/TagsFilter/TagsFilter.tsx
@@ -0,0 +1,49 @@
+// components/TagsFilter.tsx
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+export default function TagsFilter() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentTag = searchParams.get("tag");
+
+  const handleClick = (tag?: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (tag) {
+      params.set("tag", tag);
+    } else {
+      params.delete("tag");
+    }
+
+    router.push(`/?${params.toString()}`);
+  };
+
+  return (
+    <div className="tags">
+      <button
+        className={`tag_bth tag_bth--frontend ${
+          currentTag === "frontend" ? "active" : ""
+        }`}
+        onClick={() => handleClick("frontend")}
+      >
+        Frontend
+      </button>
+      <button
+        className={`tag_bth tag_bth--backend ${
+          currentTag === "backend" ? "active" : ""
+        }`}
+        onClick={() => handleClick("backend")}
+      >
+        Backend
+      </button>
+      <button
+        className={`tag_bth ${!currentTag ? "active" : ""}`}
+        onClick={() => handleClick()}
+      >
+        Сброс
+      </button>
+    </div>
+  );
+}
